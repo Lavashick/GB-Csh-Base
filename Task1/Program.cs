8bf2f658d6fab90c0a3c4f0826ba794eb33fa932
Lavashick <smesasha@mail.ru>
2019-02-15 23:20:54 +0300

    The task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фамилию: ");
+            string sername = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия: " + sername);
+            Console.WriteLine();
+
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваше имя: " + name);
+            Console.WriteLine();
+
+            Console.WriteLine("Введите отчество: ");
+            string otch = Console.ReadLine();
+            Console.WriteLine("Ваше отчество: " + otch);
+            Console.WriteLine();
+
+            Console.WriteLine("ФИО: " + sername + name + otch);
+
+            Console.ReadKey();
+        }
+    }
+}
