83104f48098fa14f98b5d7d3074b4302a370a098
Lavashick <smesasha@mail.ru>
2019-02-16 02:25:13 +0300

    The task 5 was completed with b

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -8,11 +8,30 @@ namespace Task5
 {
     class Program
     {
+        static double NormWeight (double weight, double heigth, string sex)
+        {
+            double deltaWeight;
+            if (sex == "М")
+                deltaWeight = weight - ((heigth - 100) * 1.15);
+            else
+                deltaWeight = weight - ((heigth - 110) * 1.15);
+            if (deltaWeight < 0)
+            {
+                deltaWeight *= -1;
+                Console.Write("Вам стоит набрать");
+            }
+            else
+                Console.Write("Вам стоит сбросить");
+            return Math.Round(deltaWeight, 1);
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Ваш пол М или Ж?");
+            string sex = Console.ReadLine();
             Console.WriteLine("Введите ваш вес: ");
             double weight = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine();
             Console.WriteLine("Введите ваш рост: ");
             double heigth = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine();
@@ -21,7 +40,6 @@ namespace Task5
             Console.WriteLine("Ваш ИМТ: ");
             Console.WriteLine(Math.Round(i, 4));
             Console.WriteLine();
-            Console.WriteLine();
 
             if (i < 18.5)
                 Console.WriteLine("Вам надо набрать вес");
@@ -29,6 +47,9 @@ namespace Task5
                 Console.WriteLine("Ваш вес в норме");
             if (i >= 25 && i < 30)
                 Console.WriteLine("Вам стоит похудеть");
+            Console.WriteLine();
+            Console.WriteLine(" " + NormWeight(weight, heigth, sex) + " кг");
+
 
             Console.ReadKey();
         }
