f8c8846d3d3ab192146972b2650963dcfefd96ca
Lavashick <smesasha@mail.ru>
2019-02-15 23:25:34 +0300

    The task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2).
+            // Вывести результат используя спецификатор формата .2f(с двумя знаками после запятой);
+
+            Console.WriteLine("Введите координаты точки A: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки B: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("Расстояние между точками: {0:f2}", r);
+
+            Console.ReadKey();
+        }
+    }
+}
