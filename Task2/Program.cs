e29147f150716d86cf1b4b0d554957491a476e01
Александра Раевская <Lavashik@MacBook-Pro-Artem.local>
2019-02-21 21:24:53 +0300

    The task 2 is done

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). 
+            // Требуется подсчитать сумму всех нечётных положительных чисел. 
+            // Сами числа и сумму вывести на экран, используя tryParse
+            int sum = 0;
+            do
+            {
+                string s = Console.ReadLine();
+                int value;
+                if (!Int32.TryParse(s, out value))
+                {
+                    Console.WriteLine("NaN. Вы ввели не число!");
+                    continue;
+                }
+
+                if (value == 0)
+                    break;
+                if (value % 2 != 0 && value > 0)
+                    sum += value;
+            }
+            while (true);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
